5f8f4986adace5797915e8f9ccb99a88d3639b10
Aveira <kirito.html@gmail.com>
2023-11-23 21:18:13 +0400

    all necessary packages have been created

---
+++ src/main/java/herta/kuru_kuru/kururin/KururinApplication.java
@@ -0,0 +1,13 @@
+package herta.kuru_kuru.kururin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KururinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KururinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/herta/kuru_kuru/kururin/KururinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package herta.kuru_kuru.kururin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KururinAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
